5fded2848bfc07e1e302ad9bbd7bc3ec60b6e3e7
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-10-22 22:27:06 +0700

    fix no open HTTP ports detected on 0.0.0.0

--- server.js
+++ server.js
@@ -6,28 +6,26 @@ const connectDB = require('./configs/db');
 const authRoutes = require("./routes/authRoute")
 dotenv.config();
 
-
-
 const app = express();
-
 
 // Connect DB
 connectDB();
 
-
 // Middleware
-app.use(cors);
-app.use(cookieParser);
-app.use(express.json);
+app.use(cors()); // Gọi hàm cors()
+app.use(cookieParser()); // Gọi hàm cookieParser()
+app.use(express.json()); // Gọi hàm express.json() để parse JSON
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send("Hello Express");
+});
 
 // Routes
-app.use('/', () => {
-    return "Hello Express"
-})
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
