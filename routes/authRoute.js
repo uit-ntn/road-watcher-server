40336de2b212ea260d657494907dbf0fe7bc3b3e
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-10-31 17:41:21 +0700

    fix forgot pw and reset pw

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController');
+const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 module.exports = router;
