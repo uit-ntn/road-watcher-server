1372434a42780c2c7af7d51fb738a7e30632935e
NguyenHK64 <hakhanguyen09052004@gmail.com>
2024-11-07 21:44:52 +0700

    update map api

--- server.js
+++ server.js
@@ -2,32 +2,36 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
-const connectDB = require('./configs/db');
-const authRoutes = require("./routes/authRoute")
-const potholeRoutes = require("./routes/potholeRoute")
+const { connectDB } = require('./configs/db');
+const authRoutes = require("./routes/authRoute");
+const potholeRoutes = require("./routes/potholeRoute");
+const mapRoutes = require("./routes/mapRoute");
+
 dotenv.config();
 
 const app = express();
-
-// Connect DB
-connectDB();
 
 // Middleware
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-// Test Route
-app.get('/', (req, res) => {
-    res.send("Hello Express");
-});
+// Kết nối tới MongoDB và khởi động server sau khi kết nối thành công
+connectDB().then(() => {
+    // Test Route
+    app.get('/', (req, res) => {
+        res.send("Hello Express");
+    });
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/potholes', potholeRoutes);
+    // Routes
+    app.use('/api/auth', authRoutes);
+    app.use('/api/potholes', potholeRoutes);
+    app.use('/api/map', mapRoutes);
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(err => {
+    console.error("Failed to connect to MongoDB", err);
 });
